ff5e1841e506bb0eed4650370635abba1eaa5d61
Brama Udi <bramaudi.al@gmail.com>
2020-04-26 21:44:12 +0700

    Dynamic auth links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,25 +1,26 @@
 import m from 'mithril'
-
-const navLinks = [
-  { to: '/', text: 'Index' },
-  { to: '/login', text: 'Login' },
-  { to: '/admin', text: 'Admin' }
-]
+import Auth from 'models/auth'
 
 const hideSidebar = () => {
   document.getElementById('js-sidebar').classList.toggle('active')
 }
 
+const Links = {
+  view: (vnode) => {
+    return m('li', {
+      onclick: hideSidebar
+    }, m(m.route.Link, { href: vnode.attrs.to }, vnode.children))
+  }
+}
+
+const auth = Auth.state.isLogged
+
 const Navigation = {
   view: () => (
     m('ul.nav-links', [
-
-      navLinks.map((nav, key) => (
-        m('li', {
-          onclick: hideSidebar
-        }, m(m.route.Link, { href: nav.to }, nav.text))
-      ))
-
+      m(Links, { to: '/' }, 'Home'),
+      !auth && m(Links, { to: '/login' }, 'Login'),
+      auth && m(Links, { to: '/admin' }, 'Admin')
     ])
   )
 }
