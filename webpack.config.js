2bbc2dff625af9958e49d35e06617accd612d5c2
Brama Udi <bramaudi.al@gmail.com>
2020-04-26 20:10:28 +0700

    Support current route prefix

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 3000
+    port: 3000,
+    historyApiFallback: true
   },
   module: {
     rules: [
