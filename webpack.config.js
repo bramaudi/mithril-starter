46ec58806f01713a5bfc007efa18f42bf9d9e785
Brama Udi <bramaudi.al@gmail.com>
2020-06-05 08:23:49 +0700

    update output.publicPath for working in gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
-    publicPath: '/',
+    publicPath: '',
     path: path.resolve(__dirname, './dist')
   },
   mode: 'development',
