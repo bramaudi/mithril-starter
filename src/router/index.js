f6772cb937761461067a71a8a68ea7b29b019fcf
Brama Udi <bramaudi.al@gmail.com>
2020-06-05 08:49:16 +0700

    Fix minor bug

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import auth from './_auth' // Import w/ Logged middleware
 import guest from './_guest' // Import w/ Non-logged middleware
 
 export const defaultRoutes = '/' // initial route
-export const prefix = '/mithril-starter/' // Prefix route, default: #!
+export const prefix = '/mithril-starter' // Prefix route, default: #!
 
 export const routes = {
   '/': views('index'), // views('index', false) to disable Layout
